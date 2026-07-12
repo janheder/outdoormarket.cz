28e0ae2358e07df6d93a045240a22835229276f0
Jan Heder <info@janheder.cz>
2026-07-12 03:30:27 +0200

    .

---
+++ redesign.js
@@ -0,0 +1,5 @@
+const elementKPresunu = document.querySelector('.top-navigation-bar .top-navigation-contacts');
+const novyRodic = document.querySelector('.header-top');
+if (elementKPresunu && novyRodic) {
+    novyRodic.appendChild(elementKPresunu);
+}
