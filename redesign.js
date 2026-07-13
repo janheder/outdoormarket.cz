8aa4d9884539374bc3f5ecca27a481ddc0d00675
Jan Heder <info@janheder.cz>
2026-07-13 05:17:43 +0200

    .

--- redesign.js
+++ redesign.js
@@ -1,5 +1,14 @@
-const elementKPresunu = document.querySelector('.top-navigation-bar .top-navigation-contacts');
-const novyRodic = document.querySelector('.header-top');
-if (elementKPresunu && novyRodic) {
-    novyRodic.appendChild(elementKPresunu);
+function headerPhone() {
+    const elementKPresunu = document.querySelector('.top-navigation-bar .top-navigation-contacts');
+    const novyRodic = document.querySelector('.header-top');
+    if (elementKPresunu && novyRodic) {
+        novyRodic.appendChild(elementKPresunu);
+    }
 }
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    headerPhone();  
+});
